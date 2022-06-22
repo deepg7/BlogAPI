f983c7e63d845ad18c363618867e87d4e0c7e3ef
deepg7 <deepgandhi151@gmail.com>
2022-06-22 20:00:11 +0530

    ADDED COMMENTS

--- src/routers/blog.js
+++ src/routers/blog.js
@@ -1,21 +1,28 @@
+//INITIALISING THE EXPRESS ROUTER
 const { Router } = require("express");
-const blogModel = require("../models/blog");
 const router = Router();
 
+//IMPORTING THE CONTROLLER FUNCTIONS
 const postBlog = require("../controllers/blog/post");
 const getManyBlogs = require("../controllers/blog/getManyBlogs");
 const getBlog = require("../controllers/blog/getBlog");
 const updateBlog = require("../controllers/blog/updateBlog");
 const deleteBlog = require("../controllers/blog/deleteBlog");
 
+//POST A BLOG
 router.post("/", postBlog);
 
+//GET ALL BLOGS
 router.get("/", getManyBlogs);
 
+//GET A BLOG BY ID
 router.get("/:id", getBlog);
 
+//UPDATE A BLOG BY ID USING PATCH PROTOCOL
 router.patch("/:id", updateBlog);
 
+//DELETE A BLOG BY ID
 router.delete("/:id", deleteBlog);
 
+//EXPORT ROUTER
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
+//REQUIRE CONFIGURED EXPRESS APP
 const app = require("./app");
 
+//GET CONFIGURED PORT FROM ENV VARIABLES OR ASSIGN 3000
 const PORT = process.env.PORT || 3000;
 
+//START THE SERVER ON FOUND 'PORT'
 app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
