fd33e57b34cbd17e264e6029f1e11053882eb875
deepg7 <deepgandhi151@gmail.com>
2022-06-22 19:32:18 +0530

    functionality added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
+//INITIALISING APP
 const express = require("express");
-
 const app = express();
 
-require("./db");
+//TO CONFIGURE ENVIRONMENT VARIABLES INTO PROCESS.ENV
 require("dotenv").config();
+
+//TO RUN THE FUNCTION THAT CONNECTS TO DATABASE
+require("./db");
+
+//TO GAIN ACCESS TO REQUEST BODY
 app.use(express.json());
+
+//REQUIRE ROUTER
+const blogRouter = require("./routers/blog");
+
+//USING ROUTERS
+app.use("/blog", blogRouter);
 
 module.exports = app;
