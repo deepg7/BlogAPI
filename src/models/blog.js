56468bdfd597905c98bf5acf0bfcacf444bcd26b
deepg7 <deepgandhi151@gmail.com>
2022-06-22 20:00:06 +0530

    ADDED COMMENTS

--- src/models/blog.js
+++ src/models/blog.js
@@ -1,19 +1,31 @@
+//IMPORTING MONGOOSE
 const mongoose = require("mongoose");
 
-//CONTACT SCHEMA
+//BLOG SCHEMA
 const blogSchema = new mongoose.Schema({
+  //NAME OF THE BLOG
   name: {
     type: String,
     required: true,
+    //COULD BE UNIQUE
   },
+
+  //CONTENT OF THE BLOG
   text: {
     type: String,
     required: true,
+    //COULD HAVE A MIN AND MAX LENGTH
   },
+
+  //COVER IMAGE OF THE BLOG
   cover: {
     type: String,
     required: true,
+    //MAY NOT BE REQUIRED BUT SET TO A DEFAULT BLANK IMAGE
+    //IF PROVIDED COULD BE SET AS UNIQUE USING INDEX AND PARTIALFILTEREXPRESSION ON EXISTS TRUE
   },
+
+  //AUTHOR OF THE BLOG
   author: {
     type: String,
     required: true,
@@ -22,5 +34,4 @@ const blogSchema = new mongoose.Schema({
 
 //EXPORTING MODEL
 const blogModel = mongoose.model("Blog", blogSchema);
-
 module.exports = blogModel;
